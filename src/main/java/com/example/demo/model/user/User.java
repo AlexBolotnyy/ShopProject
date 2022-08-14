4fe6aec8e6d7d01a6d96bdbe716bce2929f54a66
User <89228067141afmit@gmail.com>
2022-08-14 19:35:56 +0300

    Подключен liquidBase, требуется создание доменной модели, добавление репозиториев

--- src/main/java/com/example/demo/model/user/User.java
+++ src/main/java/com/example/demo/model/user/User.java
@@ -1,12 +1,17 @@
 package com.example.demo.model.user;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
+import lombok.Data;
+
+import javax.persistence.*;
 
 @Entity
+@Data
+@Table(name = "shop_user")
 public class User {
 
     @Column(name = "id", columnDefinition = "ID")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name", columnDefinition = "имя")
